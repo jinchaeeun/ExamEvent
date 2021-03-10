b3603f1e8881f9a5665398a86c8fe362a64af8c8
jinchaeeun <kbg05032@naver.com>
2021-03-10 12:24:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/examevent/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.examevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
